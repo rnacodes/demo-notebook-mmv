44e68c06aa10af627b7bcf8bab762d52f165dd74
Rashida A <rasanteecclesto0001@kctcs.edu>
2026-07-27 17:55:57 -0400

    Update with MMV theme

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -1,4 +1,4 @@
-import { pathToRoot } from "../util/path"
+import { joinSegments, pathToRoot } from "../util/path"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import { classNames } from "../util/lang"
 import { i18n } from "../i18n"
@@ -6,9 +6,13 @@ import { i18n } from "../i18n"
 const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
+  const logoPath = joinSegments(baseDir, "static/logo-mark.svg")
   return (
     <h2 class={classNames(displayClass, "page-title")}>
-      <a href={baseDir}>{title}</a>
+      <a href={baseDir}>
+        <img src={logoPath} alt="" class="page-title-logo" />
+        <span>{title}</span>
+      </a>
     </h2>
   )
 }
@@ -18,6 +22,18 @@ PageTitle.css = `
   font-size: 1.75rem;
   margin: 0;
   font-family: var(--titleFont);
+}
+
+.page-title a {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+}
+
+.page-title-logo {
+  height: 2.2rem;
+  width: auto;
+  flex-shrink: 0;
 }
 `
 
